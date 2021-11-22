5b044d8d4489762f9d1777c87ae07cf16f18627d
Alex An <ahn1211@gmail.com>
2021-11-21 20:18:28 -0700

    Added Some experiment on assignment

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <p> &copy; {{copy}} </p>
+  </footer>
+</template>
+<script>
+export default {
+  props: ['copy']
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,40 @@
 <template>
-  <!-- <Tutorial/> -->
-  <article>
-    1. This header is from /components/cpnt200_header.vue
-    <cpnt-200-header />
-    <br />
-    2. This is the link to the second page.<br />
-    <NuxtLink to="/second">To Second page</NuxtLink>
-    <br />
-    <br />
-    3. This is from /content/home.md using the nuxt-content tag.
-    <nuxt-content :document="page" />
-    <br />
-    4. This image optimized by the sizes attribute is from /static folder using the nuxt-img tag.
-    <nuxt-img 
-        src="/pexels-richard-verbeek-572861.jpg" 
-        sizes="sm:100vw md:50vw lg:400px"/>
-    <br />
+  <v-main>
+    <v-container>
+    <!-- <Tutorial/> -->
+    <article>
+      1. This header is from /components/cpnt200_header.vue
+      <cpnt-200-header />
+      <br />
+      2. This is the link to the second page.<br />
+      <NuxtLink to="/second">To Second page</NuxtLink><br />
+      <NuxtLink to="/blog/my-first-blog-post">To first post in blog (Dynamic page)</NuxtLink><br />
+      <NuxtLink to="/code-snippet/2021-11-21-this-is-a-code-snippet-from-the-conig-yml">To call without _slug (Test)</NuxtLink>
+      <br />
+      <br />
+      3. This is from /content/home.md using the nuxt-content tag.
+      <nuxt-content :document="page" />
+      <br />
+      4. This image optimized by the sizes attribute is from /static folder using the nuxt-img tag.
+      <nuxt-img 
+          src="/pexels-richard-verbeek-572861.jpg" 
+          sizes="sm:100vw md:50vw lg:400px"/>
+      <br />
 
-    5. This title comes from a post for blog collection. <br />
-    Title: {{ blog.title }}<br />
-    
-    6. This name comes from a profile for porfile collection. <br />
-    Name: {{ profile.name }}<br />
+      5. This title comes from a post for blog collection. <br />
+      Title: {{ blog.title }}<br />
+      
+      6. This name comes from a profile for porfile collection. <br />
+      Title: {{ profile.title }}<br />
+      <img :src="profile.picture" alt="Alex-Byung UK An" />
 
-    7. This title comes from a post for code snippet collection.
-    Title: {{ code.title }}<br />
-
-  </article>
+      7. This title comes from a post for code snippet collection.
+      Title: {{ code.title }}<br />
+      <div> {{ info }} </div>
+    </article>
+    <Footer :copy="profile.name"/>
+    </v-container>
+  </v-main>
 </template>
 <style scoped>
 a {
@@ -43,6 +51,13 @@ export default {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     };
+  },
+  data() {
+    return { 
+      info: {
+        title:'DATA TITLE TEST'
+      }
+    }
   },
   components: { cpnt200_header },
   async asyncData ({ $content }) {
